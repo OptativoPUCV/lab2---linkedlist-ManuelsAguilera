04dd18faf4f58dbf3092a7acfc888ff24ca7ca0c
Replit user <>
2023-03-23 15:54:27 +0000

    new try

--- list.c
+++ list.c
@@ -80,6 +80,8 @@ void * prevList(List * list) {
 void pushFront(List * list, void * data) {
 
   //Crea un nuevo nodo y lo intercambia por head.
+  if (list->head == NULL) return;
+  
   
   Node* newNode = createNode(data);
   newNode->next=list->head;
